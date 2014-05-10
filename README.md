c841cbcb4829fba7e9d91e08d3f663544700c3fe
SD <spencerdepas@gmail.com>
2014-05-10 13:51:58 -0400

    Now in action. Working

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
 This is my first README
 =======================
 
+Mid Or I feed now works but has a few issues.
+Currently the coridinits for the mouse position are hard coded.
+I need to make this dynamic.
+I am also working on a GUI. Some tweeking could be done.
+I allso need an input method to change the desired position.
+
+
 GUI with all singed, Jana, Draven, Morgana, Ryze
 on boot there is a random quote from one of the above toons.
--- newLOLTESTBOT.py
+++ newLOLTESTBOT.py
@@ -1,19 +1,29 @@
 import win32api
 import win32con
-import ImageGrab
 import time
 from numpy import *
+import win32com.client
 
 
+shell = win32com.client.Dispatch("WScript.Shell")
 x_pad = 75
 y_pad = 23
-boolean pressed = false
 
 
 class Cord:
     accept_match = 482, 406
     text_feild = 267, 665
     play_button = 589, 39
+
+
+def leftClick():
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, 0, 0)
+    time.sleep(.1)
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, 0, 0)
+
+
+def mousePos(cord):
+    win32api.SetCursorPos((x_pad + cord[0], y_pad + cord[1]))
 
 
 def playGame():
@@ -23,45 +33,24 @@ def playGame():
     return
 
 
-def leftClick():
-    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, 0, 0)
-    time.sleep(.1)
-    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, 0, 0)
-    print ("Click.")
-
-
-def mousePos(cord):
-    win32api.SetCursorPos((x_pad + cord[0], y_pad + cord[1]))
-
-
-def screenGrab():
-    box = (x_pad + 1, y_pad + 1, x_pad + 1151, y_pad + 719)
-    im = ImageGrab.grab(box)
-    #im.save(os.getcwd() + '\\full_snap__' + str(int(time.time())) +
-    #'.png', 'PNG')
-    textFeildColor = im.getpixel((300, 663))
-    print (textFeildColor)
-    return im
-
-
-def clickAndType():
-    print("hi I am clicked and typed")
-    s = screenGrab()
-    if s.getpixel != (2, 8, 15):
-        print("I am in the if")
-        mousePos((267, 665))
-        leftClick()
-        pressed = true
+def midOrIfeed():
+    mousePos((300, 663))
+    leftClick()
+    #time.sleep(.1)
+    #shell.SendKeys("Mid Or I Feed")
+    shell.SendKeys("Mid Please")
+    shell.SendKeys("{ENTER}")
+    time.sleep(3)
+    return
 
 
 playGame()
-while (!pressed):
-    clickAndType()
+currentTime = time.time()
+while(currentTime + 8 > time.time()):
+    midOrIfeed()
 
 
-#def main():
-    #playGame()
-    #screenGrab()
+
 
 
 
